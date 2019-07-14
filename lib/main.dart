4a29cd2e53da2d5db35bcfb8af118587de367d31
Jose Pedron Tessele <josetesselep@gmail.com>
2019-07-14 00:12:30 -0300

    Cards added

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,48 @@ import 'package:flutter/material.dart';
 
 void main() {
   runApp(MaterialApp(
-    home: Text("Hello World"),
+    home: MyStatelessWidget(),
+    debugShowCheckedModeBanner: false,
   ));
 }
+
+class MyStatelessWidget extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Stateless Widgets"),
+        centerTitle: true,
+      ),
+      body: Container(
+        padding: EdgeInsets.all(10.0),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            MyCard("I love flutter", Icons.favorite, Colors.pink),
+            MyCard("I love my phone", Icons.local_phone, Colors.blue),
+            MyCard("I love riding bike", Icons.directions_bike, Colors.black),
+          ],
+        ),
+      ),
+    );
+  }
+
+    Widget MyCard(String title, IconData icon, Color color) {
+      return Container(
+        padding: EdgeInsets.only(bottom: 15.0),
+        child: Card(
+          child: Column(
+            children: <Widget>[
+              Text(title),
+              SizedBox(height: 5.0,),
+              Icon(icon, size: 40.0, color: color,)
+            ],
+          ),
+        ),
+      );
+  }
+
+
+}
+
